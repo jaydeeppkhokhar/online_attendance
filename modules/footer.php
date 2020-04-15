64ad21c83f279fb2a0bcf75e66b3f8083856a1fc
Jaydeep Khokhar <jaydeeppkhokhar@gmail.com>
2020-04-15 23:37:06 +0530

    Initial commit

---
+++ modules/footer.php
@@ -0,0 +1,10 @@
+	</div>
+		<footer>
+			<p>&copy; 2020 mAttendance. 
+				
+			</p>
+		</footer>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/custom.js"></script>
+  </body>
+</html>
